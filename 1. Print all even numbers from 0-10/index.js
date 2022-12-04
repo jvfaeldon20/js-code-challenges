af3695b9224b38c7b0dee268616764fb79860706
JV Faeldon <jonelfaeldon@gmail.com>
2022-12-04 22:54:32 +0800

    improved challenge 1 solution

--- 1. Print all even numbers from 0-10/index.js
+++ 1. Print all even numbers from 0-10/index.js
@@ -4,8 +4,15 @@
 // While you could simply print the even numbers, get creative and see how you could 
 // output them in a way that would work up to 10 or even up to 10,000 with little extra effort.
 
-for(let x=1; x<=10; x++){
-    if(x%2 == 0){
-        console.log(x)
+function getEvenNumbers(num){
+    let even = []
+    for(let x=1; x<=num; x++){
+        if(x%2 == 0){
+            even.push(x)
+        }
     }
+
+    return even
 }
+
+console.log(getEvenNumbers(100))
