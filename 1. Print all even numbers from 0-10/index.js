8009726f535ae8e16b7d5866f52f8eabd6860497
JV Faeldon <jonelfaeldon@gmail.com>
2022-12-04 22:24:05 +0800

    solved js challenge 1

--- 1. Print all even numbers from 0-10/index.js
+++ 1. Print all even numbers from 0-10/index.js
@@ -1,2 +1,11 @@
 // CONDITION
-// Try to make the solution to this problem as efficiently as possible. Consider using loops that would allow you to type the fewest characters/commands. While you could simply print the even numbers, get creative and see how you could output them in a way that would work up to 10 or even up to 10,000 with little extra effort.
+// Try to make the solution to this problem as efficiently as possible. 
+// Consider using loops that would allow you to type the fewest characters/commands. 
+// While you could simply print the even numbers, get creative and see how you could 
+// output them in a way that would work up to 10 or even up to 10,000 with little extra effort.
+
+for(let x=1; x<=10; x++){
+    if(x%2 == 0){
+        console.log(x)
+    }
+}
